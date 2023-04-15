f5565d850dfe38dee842c398a7f47e2e42a4ca73
mina-v <mina.bessada@vodafone.com>
2023-04-15 22:17:08 +0200

    add morgan logger

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import * as path from "path";
-
-import ejs from "ejs";
+import morgan from "morgan";
 
 const app = express();
 
@@ -12,6 +11,8 @@ app.get("/", (req, res) => {
   const title = "Hello, World!";
   res.render("index.ejs", { title });
 });
+
+app.use(morgan("dev"));
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000.");
