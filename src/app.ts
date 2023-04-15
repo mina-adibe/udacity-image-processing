981d14d9dc27c6e7a95e64e6a24e7bad97ae1ecc
mina-v <mina.bessada@vodafone.com>
2023-04-15 22:15:00 +0200

    use ejs views

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import * as path from "path";
+
+import ejs from "ejs";
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  const title = "Hello, World!";
+  res.render("index.ejs", { title });
 });
 
 app.listen(3000, () => {
